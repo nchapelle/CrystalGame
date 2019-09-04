f60e652be3b1999871f05b453e8e7cf0362f4500
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2019-09-04 10:09:16 -0400

    I have the game opener working but that's it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,65 @@
 //Need dragonsEnd function
-    //end game, count total, log score, restart game?
+//end game, count total, log score, restart game?
 //treasure piles
 //
 var highscore = 0;
-var danger = 100;
+var danger = 10000;
 var roundScore = 0;
 var intervalId;
 //on load 
 
-var pressOnward = {
-    images: [],
+var dragon = {
+    images: ["assets/images/dragonKnows.gif", "assets/images/gameOver.gif"],
+    countdown: function () {
+        //start countdown 
+        //adjust based on danger
+        //alert that game is over
+        //delay inevitable
+        //empty game box 
+        // reset RoundScore 
+        console.log("...roooaaarrr...")
+    },
+    dragonDelayed: function () {
+        clearTimeout()//something goes in here-- variable that is dragon timer)
+    }
+
 };
 
 var choices = {
-    goHome: function(){
+    goHome: function () {
         var goHome = $("<div>");
         goHome.addClass("col");
+        goHome.attr("data-value", "0");
         var goHomeImg = $("<img>");
         goHomeImg.attr("src", "assets/images/cave_entranceCA.jpg");
-        goHomeImg.attr("id", "game-start");
+        goHomeImg.attr("id", "no-game");
         goHomeImg.attr("data-value", "0")
         $(goHome).append(goHomeImg);
-        $(".row").append(goHome)
+        $(".row").append(goHome);
     },
 
-    lootStart: function(){
-        var lootStart = $("<div>")
-        lootStart.addClass("col")
-        var lootStartImg = $("<img>")
-        lootStartImg.attr("src", "assets/images/golden_entry.jpg")
-        lootStartImg.attr("id", "game-start")
-        lootStartImg.attr("data-value", "1")
-        $(lootStart).append(lootStartImg)
-        $(".row").append(lootStart)
+    lootStart: function () {
+        var lootStart = $("<div>");
+        lootStart.addClass("col");
+        lootStart.attr("data-value", "1");
+        var lootStartImg = $("<img>");
+        lootStartImg.attr("src", "assets/images/golden_entry.jpg");
+        lootStartImg.attr("id", "Go-Time");
+        lootStartImg.attr("data-value", "1");
+        $(lootStart).append(lootStartImg);
+        $(".row").append(lootStart);
     },
 
-    startGame: function(){
+    startGame: function () {
         choices.goHome();
         choices.lootStart();
-        console.log("Are you a bitch?")
+        console.log("Need better styling")
     }
 
 
-    
+
 };
 
-// var dragonHome = {
-//     burnTreasure: function(){
-//         $("#game-box").empty()
-//     },
-
-// };
 
 var treasurePile = {
     name: ["Cave Entrance", "Gold Coins", "Gemstones", "Fine Jewelry"],
@@ -59,92 +68,120 @@ var treasurePile = {
     description: ["Get out while you still can!", "A handful of gold coins.", "A sparkling gemstone.", "Jewelry fit for royalty."],
     action: ["Leave", "Min Gain", "Med Gain", "Max Gain"],
     danger: 0,
-    lootPiles: function() {
+    lootPiles: function () {
         for (var i = 0; i < treasurePile.images.length; i++) {
             var x = [Math.floor((Math.random() * 4))];
-    
-              var lootBox = $("<div>");
-              lootBox.addClass("col-xs-12 col-med-6 col-lg-3");
-              
-              var lootCard = $("<div>");
-              lootCard.addClass("card text-center");
-              $(lootBox).append(lootCard);
-              $(".row").append(lootBox);
-    
-              var lootImg = $("<img>");
-              lootImg.addClass("card-image-top");
-              lootImg.attr("id", "lootPics");
-              lootImg.attr("src", treasurePile.images[x]);
-              lootImg.attr("data-value", treasurePile.value[x]);
-              $(lootCard).append(lootImg);
-    
-              var lootBody = $("<div>");
-              lootBody.addClass("card-body");
-              
-              var bodyH5 = $("<h5>");
-              bodyH5.addClass("card-title");
-              bodyH5.text(treasurePile.name[x]);
-    
-              var bodyText = $("<p>");
-              bodyText.addClass("card-text");
-              bodyText.text(treasurePile.description[x])
-              var bodyBtn = $("<buttons>");
-              bodyBtn.addClass("btn btn-primary");
-              bodyBtn.attr("data-value", treasurePile.value[x]);
-              bodyBtn.text(treasurePile.action[x]);
-              $(lootBody).append(bodyH5, bodyText, bodyBtn);
-              $(lootCard).append(lootBody);
-             
-              
-          };
+
+            var lootBox = $("<div>");
+            lootBox.addClass("col-xs-12 col-med-6 col-lg-3");
+
+            var lootCard = $("<div>");
+            lootCard.addClass("card text-center");
+            $(lootBox).append(lootCard);
+            $(".row").append(lootBox);
+
+            var lootImg = $("<img>");
+            lootImg.addClass("card-image-top-center");
+            lootImg.attr("id", "lootPics");
+            lootImg.attr("src", treasurePile.images[x]);
+            lootImg.attr("data-value", treasurePile.value[x]);
+            $(lootCard).append(lootImg);
+
+            var lootBody = $("<div>");
+            lootBody.addClass("card-body");
+
+            var bodyH5 = $("<h5>");
+            bodyH5.addClass("card-title");
+            bodyH5.text(treasurePile.name[x]);
+
+            var bodyText = $("<p>");
+            bodyText.addClass("card-text");
+            bodyText.text(treasurePile.description[x])
+            var bodyBtn = $("<buttons>");
+            bodyBtn.addClass("btn btn-primary");
+            bodyBtn.attr("data-value", treasurePile.value[x]);
+            bodyBtn.attr("id", "next-round")
+            bodyBtn.text(treasurePile.action[x]);
+            $(lootBody).append(bodyH5, bodyText, bodyBtn);
+            $(lootCard).append(lootBody);
+
+
+        };
     },
 
-    
+    lootValue: function(){
+        console.log("Nice Click.");
+    },
+
+
 };
 
-onLoad = choices.startGame(), 
-$(document).ready(function() {
+onLoad = choices.startGame(),
+    $(document).ready(function () {
 
-      // 1. Create a for-loop to iterate through the letters array.
+        $("#next-round").on("click", function(){
+            console.log("nice click")
+        });
 
-      $(".btn").on("click", function(){
-          var treasureValue = ($(this).attr("data-value"));
-          treasureValue = parseInt(treasureValue);
-          roundScore += treasureValue
-          console.log("Treasure Value: " + treasureValue);
-          console.log("Round Score: " + roundScore);
+        $("#Go-Time").on("click", function () {
+            console.log("we are clicking");
 
-          if (treasureValue === 0){
-            if (roundScore > highscore) {          
-            highscore = roundScore
-            alert("New High Score. Take a Screenshot! High Score: " + highscore )
-            $("#highscore").text("High Score: " + highscore)
-            }
-        else {
-            alert("You escaped with your treasures. Your score is: " + roundScore)
-        }
-        };
-        $("#game-box").empty();
-        
-      });
-    $(".col").on("click", function(){
-        var startValue = ($(this).attr("data-value"));
-        startValue = parseInt(startValue);
-        if (startValue === 1) {
-            $(".row").empty()
+            $(".row").empty();
+            console.log("Correct Choice, game start!");
             treasurePile.lootPiles();
-            console.log("Correct Choice, game start!")
 
-        }
-        else {
-            $(".row").empty()
-            console.log("Good Job.")
 
-        }
-        //if start run game
-        //if quit show 
+        });
+
+        $("#no-game").on("click", function () {
+            delayButtonAlert = setTimeout(function () {
+                alert("You have wasted your opportunity.");
+                $(".row").empty();
+            }, 1000);
+           
+        });
+        // var treasureValue = ($(this).attr("data-value")); //something is wrong with this function, I believe that values are not being read properly it could be the if/else arguments. 
+        // treasureValue = parseInt(treasureValue);
+        // roundScore += treasureValue;
+        // console.log("Treasure Value: " + treasureValue);
+        // console.log("Round Score: " + roundScore);
+
+        // if (treasureValue === 0) { console.log("Cave Entrance Clicked.")
+        //     if (roundScore > highscore) { console.log("Is this your first time here?")
+        //         highscore = roundScore;
+        //         alert("New High Score. Take a Screenshot! High Score: " + highscore)
+        //         $("#highscore").text("High Score: " + highscore)
+        //         //delay 
+        //         $("#game-box").empty();
+        //         choices.startGame();
+        //     }
+        //     else {
+        //         alert("You escaped with your treasures. Your score is: " + roundScore)
+        //         $("#game-box").empty();
+        //         choices.startGame();
+        //     }
+
+
+
+        // }
+        // else if (treasureValue === 5) {
+        //         console.log("Greedy.");
+        //         danger++;
+        //         //roundchecker;
+        //         }
+        // else {
+        //         if (roundScore > 5) {
+        //             danger++
+        //             //roundchecker;
+        //         }
+        //         else {
+        //             console.log("You sneaky bastard.");
+        //         };
+        //     };
+
+
+
     });
-});
 
     //   <div class="row">
     //   <div class="col-xs-12 col-lg-3" id="game-box">
